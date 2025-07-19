13a7a53c9506654ff3bae2452aa223a6e2b0fc76
MohamedAhmedDev <mohamedahmeddev333@gmail.com>
2025-07-19 21:20:21 +0300

    First commit

---
+++ database/migrations/2025_07_19_181228_create_user_profiles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_profiles', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign("user_id")->references("id")->on("users");
+            $table->string("username");
+            $table->text("profile_image")->nullable();
+            $table->enum("current_level", ["beginner", "intermediate", "advanced"]);
+            $table->integer("verses_memorized_count")->default(0);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_profiles');
+    }
+};
