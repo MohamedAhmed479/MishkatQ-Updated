fe6b4ebb011871fdc323065524108a2a83a331da
MohamedAhmedDev <mohamedahmeddev333@gmail.com>
2025-09-04 14:25:17 +0300

    Fix case sensitivity for Api folder

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -3,12 +3,12 @@
 
 // Load all authentication-related routes for both regular users and admins.
 // These routes are defined in: routes/api/V1/auth.php
-require __DIR__ . '/api/V1/auth.php';
+require __DIR__ . '/Api/V1/auth.php';
 
 
 // Load all routes related to user management.
 // These routes are defined in: routes/api/V1/user.php
-require __DIR__ . '/api/V1/user.php';
+require __DIR__ . '/Api/V1/user.php';
 
 
-require __DIR__ . '/api/V1/quran.php';
+require __DIR__ . '/Api/V1/quran.php';
