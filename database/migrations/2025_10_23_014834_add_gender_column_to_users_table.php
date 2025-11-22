ec3a5a177933a001d4bae4e49a4dc7043865f37a
asmaasaleh185 <asmaasaleh185@gmail.com>
2025-11-22 21:59:31 +0200

    feat(migration): Make gender column nullable in users table migration

--- database/migrations/2025_10_23_014834_add_gender_column_to_users_table.php
+++ database/migrations/2025_10_23_014834_add_gender_column_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->enum('gender', ['female', 'male'])->after('email');
+            $table->enum('gender', ['female', 'male'])->nullable()->after('email');
         });
     }
 
